de1f025ef32eaf5ba9854370f98a92a2d74a3b11
JmArellano554 <arellanojuan554@gmail.com>
2021-12-10 11:38:51 -0300

    subiendo Aplicación Node.js+Express con archivos html

---
+++ views/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+app.listen(3030, () =>{
+    console.log('servidor corriendo en el puerto 3030');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
