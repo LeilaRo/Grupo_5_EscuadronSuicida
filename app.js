8060146a38cc4a8a18b1cbaab93801cb68184dc8
DiegoSCC <diego.santino03@gmail.com>
2022-01-06 20:28:00 -0300

    Implementacion de EJS

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const express = require('express');
+const express = require("express");
 
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
+app.set("view engine", "ejs");
+
+const publicPath =  "public";
+app.use(express.static(publicPath) );
 
 app.listen(3030, () =>{
-    console.log('servidor corriendo en el puerto 3030');
+    console.log("servidor corriendo en el puerto 3030");
 })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+app.get("/", (req, res) => {
+    res.render( "index");
 })
 
-app.get('/productCart', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productCart.html'));
+app.get("/productCart", (req, res) => {
+    res.render( "productCart");
 })
-app.get('/login', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/login.html'));
-})
-
-app.get('/productDetail', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+app.get("/login", (req, res) => {
+    res.render("login");
 })
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/register.html'));
+app.get("/productDetail", (req, res) => {
+    res.render( "productDetail");
+})
+
+app.get("/register", (req, res) => {
+    res.render( "register");
     
 })
