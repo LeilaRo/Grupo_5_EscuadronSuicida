cd1066fdd897bc53458e7b87c7a86454acbf6776
LeilaRo <leilarobledo@outlook.com>
2022-01-21 11:08:24 -0300

    saveProduct en proceso

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 app.use(express.static(publicPath)); 
 
 app.use(express.urlencoded({extended:false}));//post
-app.use(express.json);
+app.use(express.json());
 
 app.use(methodOverride('_method'))
 
--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,4 +1,11 @@
 const path= require('path');
+const fs = require('fs');
+
+//Leer el archivo JSON de productos:
+const productsFilePath = path.join(__dirname, '../data/products.json')
+const jsonProducts = fs.readFileSync(productsFilePath, "utf-8");
+//Traducir ese archivo json: luego lo usaremos en saveProduct para guardar los productos que se crean
+const products = JSON.parse(jsonProducts);
 
 const controlador={
     index: function(){},
@@ -10,6 +17,25 @@ const controlador={
     createProduct: (req, res) => {
         res.render( "products/createProduct");
     },
+    saveProduct: (req, res) =>{
+        //Obtener los datos de req.body
+        let newProduct={
+                product: req.body.product,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
+            } 
+        console.log(newProduct)
+        //Escribir esos datos en json, usar fs y escribirlo en el archivo:
+        products.push(newProduct);
+        const newJsonProduct = JSON.stringify(products);
+    
+        fs.writeFileSync(productsFilePath, newJsonProduct, "utf-8")
+        console.log("se guardo el producto")
+    
+            //res.redirect({ruta a donde quiero que se redirija ej: "/products"})
+        res.redirect("/productDetail");
+        },
     
     editProduct: (req, res) => {
         res.render('products/editProduct');
@@ -19,11 +45,7 @@ const controlador={
     productsList: (req, res)=> {
     },
 //Leila
-    saveProduct: (req, res) =>{
-        //Obtener los datos de req.body
-        //Escribir esos datos en json, usar fs y escribirlo en el archivo
-        //res.redirect({ruta a donde quiero que se redirija ej: "/products"})
-    },
+   
     saveEditProduct: (req, res) =>{
 
     },
